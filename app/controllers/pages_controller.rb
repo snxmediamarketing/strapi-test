de727208ff114b45fa68b9911595c4cf0f2f0ca0
Nicholas <berubenic@gmail.com>
2023-08-11 16:19:33 -0400

    update api endpoint and url.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    response = HTTParty.get("http://localhost:1337/api/services/", format: :json)
+    response = HTTParty.get("http://167.172.149.31:1337/api/services", format: :json)
     @body = response.parsed_response
   end
 end
